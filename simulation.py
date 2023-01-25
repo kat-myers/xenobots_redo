60ae2a607fe42ef4013ac0ad86532d81bd6f03b6
kat-myers <kathrynmyers2027@u.northwestern.edu>
2023-01-25 14:45:12 -0600

    simulation

--- simulation.py
+++ simulation.py
@@ -1,27 +1,32 @@
-import numpy
+#Imports
+import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy as np
 import random
 import constants as c
-import pybullet as p
-import time
 from world import WORLD
-from sensor import SENSOR
 from robot import ROBOT
+from sensor import SENSOR
+from motor import MOTOR
 
-class SIMULATION: #names class
-    def __init__(self): # defines constructor for class
-        self.world = WORLD()
-        self.robot = ROBOT()
-    
+class SIMULATION:
+    def __init__(self):
+            self.world = WORLD()
+            self.robot = ROBOT()
+
     def Run(self):
         for i in range(c.iterations):
             p.stepSimulation()
+            
             self.robot.Sense(i)
-            self.robot.Think(i)
+            self.robot.Think()
             self.robot.Act(i)
-            time.sleep(c.sleep_time)
-    
+            
+            time.sleep(c.sleep)
+                        
     def __del__(self):
-        p.disconnect
+        #SENSOR.Save_Values()
+        #MOTOR.Save_Values()
+        p.disconnect()
