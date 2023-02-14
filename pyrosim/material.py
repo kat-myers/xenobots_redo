ca34b18d3ae370db03cc2e81d237bf685f1e70e4
kat-myers <122335561+kat-myers@users.noreply.github.com>
2023-02-13 21:55:11 -0600

    Update material.py

--- pyrosim/material.py
+++ pyrosim/material.py
@@ -2,15 +2,24 @@ from pyrosim.commonFunctions import Save_Whitespace
 
 class MATERIAL: 
 
-    def __init__(self):
+    def __init__(self,x):
 
         self.depth  = 3
 
-        self.string1 = '<material name="Cyan">'
+        if x == 0:
+            self.string1 = '<material name="Cyan">'
 
-        self.string2 = '    <color rgba="0 1.0 1.0 1.0"/>'
+            self.string2 = '    <color rgba="0 1.0 1.0 1.0"/>'
 
-        self.string3 = '</material>'
+            self.string3 = '</material>'
+        
+        if x == 1:
+            self.string1 = '<material name="Green">'
+
+            self.string2 = '    <color rgba="0 1.0 0 1.0"/>'
+
+            self.string3 = '</material>'
+
 
     def Save(self,f):
 
